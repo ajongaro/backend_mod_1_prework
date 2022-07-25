9353b7f050b8f4f86f0b54f9287f38f26bd4ffaf
Anthony Ongaro <aongaro@gmail.com>
2022-07-25 09:56:12 -0700

    feat: added class structure to fizzbuzz

--- day_7/fizzbuzz.rb
+++ day_7/fizzbuzz.rb
@@ -1,32 +1,39 @@
 # the fizzbuzz solve
 
-# create an array of numbers 1 - 100, inclusive
-numbers = (1..100).to_a
-# new array for resulting data
-result = []
+class FizzBuzz
+  attr_accessor :numbers, :result
 
-# each loop and flow control
-numbers.each do |x|
-  if x % 3 == 0 && x % 5 == 0
-    result << "FizzBuzz"
-  elsif x % 3 == 0
-    result << "Fizz"
-  elsif x % 5 == 0
-    result << "Buzz"
-  else
-    result << x
+  def initialize
+    @numbers = (1..100).to_a
+    @result = []
   end
+
+  def generator
+    numbers.each do |x|
+      if x % 3 == 0 && x % 5 == 0
+        result << "FizzBuzz"
+      elsif x % 3 == 0
+        result << "Fizz"
+      elsif x % 5 == 0
+        result << "Buzz"
+      else
+        result << x
+      end
+    end
+  end
+
+  def printer
+    @result.each_with_index do |x, index|
+      unless index == @result.size - 1
+        print "#{x}, "
+      else
+        print "#{x}."
+      end
+    end
+  end
+
 end
 
-# print array (simple)
-p result
-
-# iterate and put a period at the end instead of ", "
-result.each_with_index do |x, index|
-  # unless the index of elemnt being printed is the same as the size of the array minus one (the last element)
-  unless index == result.size - 1
-    print "#{x}, "
-  else
-    print "#{x}."
-  end
-end
+project = FizzBuzz.new
+project.generator
+project.printer
